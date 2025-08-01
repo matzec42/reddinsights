d158fc86686ff0c54c6720deca7f60aaaffbe847
Chris M <matzec42@gmail.com>
2025-07-31 17:25:02 -0700

    Started traditional auth; pseudo/skeleton code PW hashing, salting functions; additional folders created

---
+++ reddinsights-app/src/auth/core/passwordHasher.ts
@@ -0,0 +1,17 @@
+import crypto from "crypto";
+
+// 
+export function hashPassword(password: string, salt: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        crypto.scrypt(password.normalize(), salt, 64, (error, hash) => {
+            if (error) reject(error);
+            // converts to storage-friendly string (hex and normalized)
+            resolve(hash.toString("hex").normalize());
+        })
+    })
+}
+
+// function to generate random salt strings
+export function generateSalt() {
+    return crypto.randomBytes(16).toString("hex").normalize();
+}
