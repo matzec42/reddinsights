fa9f3e64d2de75de045ded3207663ec463dec1dc
Chris M <matzec42@gmail.com>
2025-08-01 15:27:04 -0700

    Minor styling

--- reddinsights-app/src/app/dashboard/layout.tsx
+++ reddinsights-app/src/app/dashboard/layout.tsx
@@ -2,9 +2,6 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div>
-            <div>
-                Hello World
-            </div>
             <div>{children}</div>
         </div>
     )
